250ee4b9a564158694140cc3f66b67cc38230fe2
gumglegee <gz4have@hotmail.com>
2014-09-30 12:44:31 -0400

    lab4 done

--- Comp15/2014Fall/lab4/sandwichList.cpp
+++ Comp15/2014Fall/lab4/sandwichList.cpp
@@ -38,7 +38,16 @@ Order SandwichList::dequeue()
 {
 
 // TODO: Students write code here
+	Order fOrder = orders[front];
 
+	if (front == capacity-1)
+		front = 0;
+	else
+		front++;
+
+	orderCount--;
+
+	return fOrder;
 }
 
 //add an element, make sure it is not full, if it is call expand funciton
@@ -46,7 +55,18 @@ void SandwichList::enqueue(const Order& sw)
 {
 
 // TODO: Students write code here
+	//expend the queue if it is full
+	if (capacity == orderCount)
+		expandList();
+
+	orders[back] = sw;
+
+	if (back == capacity-1)
+		back = 0;
+	else
+		back++;
 	
+	orderCount++;
 }
 
 //Double the queue size, copy the values, and reset back and front
@@ -54,6 +74,19 @@ void SandwichList::expandList()
 {
 
 // TODO: Students write code here
+	Order* newOrders = new Order[capacity * 2];
+
+	for (int i=0; i < capacity; i++) {
+		newOrders[i] = orders[(front+i)%capacity];
+	}
+
+	delete [] orders;
+
+	orders = newOrders;
+
+	front = 0;
+	back = capacity;
+	capacity *= 2;
 
 }
 

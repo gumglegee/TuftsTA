96583ff7fcaa3db86abcadf8fe2af4df3a05cea2
gumglegee <gz4have@hotmail.com>
2014-09-30 15:26:40 -0400

    add function head in Queue.cpp file, no body

--- Comp15/2014Fall/hw3/Queue.cpp
+++ Comp15/2014Fall/hw3/Queue.cpp
@@ -6,4 +6,32 @@
 #include <iostream>
 #include "Queue.h"
 
-// TODO: Student writes this class
+Queue::Queue() {
+
+}
+
+// destructor
+Queue::~Queue() {
+
+}
+
+// enqueue a char onto at the back of the queue
+void Queue::enqueue(Student c) {
+
+}
+
+// dequeue a char from the front of the queue
+Student Queue::dequeue() {
+
+}
+
+// returns true if there are no elements in the
+// queue, false if the queue has elements
+bool Queue::is_empty() {
+
+}
+
+// expand the circular queue
+void Queue::expand() {
+
+}

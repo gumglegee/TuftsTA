341d3438c66772348d82b69f5a69e399e9dccb12
gumglegee <gz4have@hotmail.com>
2014-11-18 15:34:35 -0500

    ADD: WordList class

---
+++ Comp15/2014Fall/hw6/WordList.h
@@ -0,0 +1,18 @@
+/*
+ * WorldList.h
+ *
+ *  Created on: Nov 18, 2014
+ */
+
+#ifndef WORLDLIST_H_
+#define WORLDLIST_H_
+
+class WordList {
+public:
+	//constructor
+
+
+};
+
+
+#endif /* WORLDLIST_H_ */
